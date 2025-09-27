2fca1226c1aacd04363520f51ff15692c952c5d4
fray osorio <frayosorio@gmail.com>
2025-09-27 08:31:14 -0500

    Se actualizan consultas para probar SP

--- TRIGGER TiendaVideojuegos.sql
+++ TRIGGER TiendaVideojuegos.sql
@@ -1,23 +1,3 @@
---Agregar campo para registrar las existencias para las ventas
-ALTER TABLE Titulo
-    ADD Existencia INT NOT NULL DEFAULT 0;
-
---Actualizar el campo EXISTENCIA en un valor inicial
-UPDATE Titulo
-	SET Existencia = 10
-
---Consultar EXISTENCIA
-SELECT Id, Nombre, Existencia
-	FROM Titulo
-
---Consulta de Prueba: Agregar un Detalle de una Venta
-INSERT INTO VentaDetalle (IdVenta, IdTitulo, Cantidad, Precio, Descuento) 
-	VALUES(1, 1, 2, 75000, 0);
-
-INSERT INTO VentaDetalle (IdVenta, IdTitulo, Cantidad, Precio, Descuento) 
-	VALUES(2, 1, 1, 75000, 0),
-	(2, 2, 2, 100000, 0);
-
 --Crear un TRIGGER que actualice la existencia de TITULOS despues de hacer una VENTA
 CREATE TRIGGER tActualizarExistenciaTitulo
 ON VentaDetalle
